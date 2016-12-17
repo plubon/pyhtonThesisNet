c9e252e3fa572920492a46a6bd574c42666a8cde
262698 lubon piotr <plubon1337@gmail.com>
2016-12-17 17:30:45 +0100

    learning rate decay

--- network.py
+++ network.py
@@ -9,38 +9,43 @@ class network:
     reportFrequency = 50
 
     def __init__(self):
-        self.dropoutRate = tf.placeholder(tf.float32)
-        self.session = None
-        self.dataHelper = None
-        self.layers = []
-        self.layers.append(layers.conv(7, 96, 50, 1))
-        self.layers.append(layers.maxPool(2))
-        self.layers.append(layers.conv(5, 192, 96, 2))
-        self.layers.append(layers.maxPool(2))
-        self.layers.append(layers.conv(3, 512, 192, 1))
-        self.layers.append(layers.maxPool(2))
-        self.layers.append(layers.conv(2, 4096, 512, 1))
-        self.layers.append(layers.dense(3*4096, 4096, dropout_rate=self.dropoutRate, reshape_needed=True))
-        self.layers.append(layers.dense(4096, 2048, dropout_rate=self.dropoutRate))
-        self.layers.append(layers.dense(2048, 2))
-        self.input = tf.placeholder(tf.float32, [None, 60, 40, 50])
-        self.normalizedInput = tf.truediv(tf.sub(self.input, tf.constant(128.)), tf.constant(128.))
-        self.results = []
-        self.results.append(self.layers[0].result(self.normalizedInput))
-        for i in xrange(1, len(self.layers)):
-            try:
-                self.results.append(self.layers[i].result(self.results[i-1]))
-            except:
-                print(i)
-                raise
-        self.finalResult = self.results[len(self.results) - 1]
-        self.labels = tf.placeholder(tf.float32, [None, 2])
-        self.crossEntropy = tf.reduce_mean(
-            tf.nn.softmax_cross_entropy_with_logits(self.finalResult, self.labels))
-        self.train_step = tf.train.GradientDescentOptimizer(1e-3).minimize(self.crossEntropy)
-        self.correct_prediction = tf.equal(tf.argmax(self.finalResult, 1), tf.argmax(self.labels, 1))
-        self.accuracy = tf.reduce_mean(tf.cast(self.correct_prediction, tf.float32))
-        self.saver = None
+		self.global_step = tf.Variable(0, trainable=False)
+		self.starter_learning_rate = 0.004
+		self.end_learning_rate = 0.0001
+		self.decay_steps = 100
+		self.learning_rate = tf.train.polynomial_decay(self.starter_learning_rate, self.global_step, self.decay_steps, self.end_learning_rate, power=0.5)
+		self.dropoutRate = tf.placeholder(tf.float32)
+		self.session = None
+		self.dataHelper = None
+		self.layers = []
+		self.layers.append(layers.conv(7, 96, 50, 1))
+		self.layers.append(layers.maxPool(2))
+		self.layers.append(layers.conv(5, 192, 96, 2))
+		self.layers.append(layers.maxPool(2))
+		self.layers.append(layers.conv(3, 512, 192, 1))
+		self.layers.append(layers.maxPool(2))
+		self.layers.append(layers.conv(2, 4096, 512, 1))
+		self.layers.append(layers.dense(3*4096, 4096, dropout_rate=self.dropoutRate, reshape_needed=True))
+		self.layers.append(layers.dense(4096, 2048, dropout_rate=self.dropoutRate))
+		self.layers.append(layers.dense(2048, 2))
+		self.input = tf.placeholder(tf.float32, [None, 60, 40, 50])
+		self.normalizedInput = tf.truediv(tf.sub(self.input, tf.constant(128.)), tf.constant(128.))
+		self.results = []
+		self.results.append(self.layers[0].result(self.normalizedInput))
+		for i in xrange(1, len(self.layers)):
+			try:
+				self.results.append(self.layers[i].result(self.results[i-1]))
+			except:
+				print(i)
+				raise
+		self.finalResult = self.results[len(self.results) - 1]
+		self.labels = tf.placeholder(tf.float32, [None, 2])
+		self.crossEntropy = tf.reduce_mean(
+			tf.nn.softmax_cross_entropy_with_logits(self.finalResult, self.labels))
+		self.train_step = tf.train.GradientDescentOptimizer(self.learning_rate).minimize(self.crossEntropy)
+		self.correct_prediction = tf.equal(tf.argmax(self.finalResult, 1), tf.argmax(self.labels, 1))
+		self.accuracy = tf.reduce_mean(tf.cast(self.correct_prediction, tf.float32))
+		self.saver = None
 
     def test(self, path):
         self.session = tf.InteractiveSession()
